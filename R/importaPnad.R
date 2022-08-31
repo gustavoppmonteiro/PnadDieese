3b9c0419df96a4fd629e801bea72db4c11731bfd
Gustavo Monteiro <gustavoppmonteiro@gmail.com>
2022-08-31 00:34:38 -0300

    exemplo v3

--- R/importaPnad.R
+++ R/importaPnad.R
@@ -23,18 +23,21 @@
 #' library(PNADcIBGE)
 #'
 #'
+#'
 #' # seleciona variaveis
-#' variaveis <-  c("V1028", "Ano", "Trimestre", "UF", "V2007")
+#' variaveis <-  c("V1028", "Ano", "Trimestre", "UF", "V2007", "VD4002")
 #'
-#' # importa base
-#' dados_2T22 <- importaPnad(2, 2022, lista_var = variaveis)
+#' dados_2T22 <- PnadDieese::importaPnad(2, 2022, lista_var = variaveis)
 #'
 #'
-#' # tabela com frequencia por sexo
 #' T_1 <- dados_2T22 %>%
-#'       faz_tab_freq(., V2007)
+#'       filter(VD4002==1) %>%
+#'       PnadDieese::faz_tab_freq(., V2007)
 #'
 #' T_1
+
+
+
 
 
 
